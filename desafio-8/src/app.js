fce860b73430ae9f8995964fdb8dd3c77db429d8
lucianocaro1995 <lucho_lp_1995@hotmail.com>
2023-09-24 17:35:00 -0300

    Creé un readme y limpié de comentarios el archivo app.js

--- desafio-8/src/app.js
+++ desafio-8/src/app.js
@@ -1,20 +1,3 @@
-/*
-IMPORTANTE: Debo ejecutar "npm run dev" en la terminal para poder ver el localhost:4000 en mi navegador
-Para este desafío instalamos 4 dependencias:
-1) Instalo "npm i dotenv"
-Nos permite manejar variables de entorno dentro de nuestra aplicación. Sirve para ocultar contraseñas y no subirlas a github
-Copio toda la URL que escribí en la conexión con MongoDB Atlas y la pego en el archivo ".env". Luego donde estaba la URL pongo "process.env.MONGO_URL"
-En la versión nueva de node lo tenemos de forma nativa, pero en esta versión tengo que instalando desde la terminal
-2) Instalo "npm i cookie-parser"
-Nos permite trabajar con cookies. Cookies: formas de guardar información en el cliente
-3) Instalo "npm i express-session"
-Nos permite manejar las sesiones de mi aplicación que se guardan en el servidor, no en la base de datos. No tiene nada que ver con las cookies
-4) Instalo "npm i connect-mongo"
-Nos permite trabajar con sesiones guardadas en la base de datos MongoDB (ya que guardarlas en un servidor no es recomendable porque se caen seguido)
-*/
-
-
-
 //1) Imports
 //Módulos
 import 'dotenv/config'
@@ -64,25 +47,21 @@ mongoose.connect(process.env.MONGO_URL)
 //Middlewares
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser(process.env.SIGNED_COOKIE)) //La cookie esta firmada
+app.use(cookieParser(process.env.SIGNED_COOKIE))
 app.use(session({
     //1) Configuración de sesiones en la base de datos MongoDB
     store: MongoStore.create({
         mongoUrl: process.env.MONGO_URL,
         mongoOptions: {
-            useNewUrlParser: true, //Establezco que la conexión sea mediante URL
-            useUnifiedTopology: true //Manejo de clusters de manera dinámica
+            useNewUrlParser: true,
+            useUnifiedTopology: true
         },
-        ttl: 60 //Duracion de la sesión en la base de datos en segundos
-
+        ttl: 60
     }),
+    
     //2) Configuración de sesiones en el servidor
     secret: process.env.SESSION_SECRET,
-    //Resave permite mantener la sesión activa en caso de que la sesión se mantenga inactiva
-    //Si se deja en false, la sesión morirá en caso de que exista cierto tiempo de inactividad
     resave: false,
-    //saveUninitialized permite guardar cualquier sesión aún cuando el objeto de sesión no tenga nada por contener
-    //Si se deja en false, la sesión no se guardará si el objeto de sesión está vacío al final de la consulta
     saveUninitialized: false
 }))
 
@@ -152,7 +131,6 @@ app.get('/logOut', (req, res) => {
 
 
 //6) Socket.io
-//Para trabajar en tiempo real
 const io = new Server(serverExpress);
 
 io.on('connection', (socket)=> {
